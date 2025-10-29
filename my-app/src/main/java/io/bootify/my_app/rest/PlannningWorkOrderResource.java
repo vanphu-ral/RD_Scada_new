291e38160d968e20869fcc3a73a99c12f4e9be76
nkhieu215 <102019148+nkhieu215@users.noreply.github.com>
2025-10-29 16:36:52 +0700

    update

--- my-app/src/main/java/io/bootify/my_app/rest/PlannningWorkOrderResource.java
+++ my-app/src/main/java/io/bootify/my_app/rest/PlannningWorkOrderResource.java
@@ -1,9 +1,12 @@
 package io.bootify.my_app.rest;
 
+import io.bootify.my_app.model.PlanningWorkOrderResponse;
 import io.bootify.my_app.service.PlanningWorkOrderService;
 import io.bootify.planning.domain.PlanningWorkOrder;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -43,5 +46,17 @@ public ResponseEntity<Page<PlanningWorkOrder>> getLatestWorkOrders(
     @GetMapping("/byProductionOrderId")
     public List<PlanningWorkOrder> getByProductionOrderId(@RequestParam String productOrderId) {
         return planningWorkOrderService.getByPRODUCT_ORDER_ID(productOrderId);
+    }
+    @GetMapping
+    public Page<PlanningWorkOrderResponse> getPlanningWorkOrders(
+            @RequestParam(required = false) String woId,
+            @RequestParam(required = false) String productCode,
+            @RequestParam(required = false) String sapWoId,
+            @RequestParam(required = false) String lotNumber,
+            @RequestParam(defaultValue = "0") int page,
+            @RequestParam(defaultValue = "10") int size
+    ) {
+        Pageable pageable = PageRequest.of(page, size);
+        return planningWorkOrderService.searchWorkOrders(woId, productCode, sapWoId, lotNumber, pageable);
     }
 }
--- my-app/src/main/java/io/bootify/planning/repos/PlanningWorkOrderRepository.java
+++ my-app/src/main/java/io/bootify/planning/repos/PlanningWorkOrderRepository.java
@@ -1,5 +1,6 @@
 package io.bootify.planning.repos;
 
+import io.bootify.my_app.model.PlanningWorkOrderResponse;
 import io.bootify.planning.domain.PlanningWorkOrder;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
@@ -7,6 +8,7 @@ import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.domain.Specification;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
@@ -17,6 +19,31 @@ public interface PlanningWorkOrderRepository extends JpaRepository<PlanningWorkO
     List<String> findAllProductionOrderIds();
 @Query(value = "select * from planning_db.planning_work_order where  PRODUCT_ORDER_ID = ?1 ;",nativeQuery = true)
 List<PlanningWorkOrder> getByPRODUCT_ORDER_ID(String productOrderId);
+    @Query(
+            value = "SELECT a.*, b.customer_code AS customerCode, b.customer_name AS customerName " +
+                    "FROM planning_db.planning_work_order a " +
+                    "LEFT JOIN planning_db.product_order_item b " +
+                    "ON b.product_code = a.product_code AND b.product_order_id = a.product_order_id AND b.bom_version = a.bom_version " +
+                    "WHERE (:woId IS NULL OR a.wo_id LIKE %:woId%) " +
+                    "AND (:productCode IS NULL OR a.product_code LIKE %:productCode%) " +
+                    "AND (:sapWoId IS NULL OR a.sap_wo_id LIKE %:sapWoId%) " +
+                    "AND (:lotNumber IS NULL OR a.lot_number LIKE %:lotNumber%)",
+            countQuery = "SELECT COUNT(*) FROM planning_db.planning_work_order a " +
+                    "WHERE (:woId IS NULL OR a.wo_id LIKE %:woId%) " +
+                    "AND (:productCode IS NULL OR a.product_code LIKE %:productCode%) " +
+                    "AND (:sapWoId IS NULL OR a.sap_wo_id LIKE %:sapWoId%) " +
+                    "AND (:lotNumber IS NULL OR a.lot_number LIKE %:lotNumber%)",
+            nativeQuery = true
+    )
+    Page<PlanningWorkOrderResponse> findAllProjected(
+            @Param("woId") String woId,
+            @Param("productCode") String productCode,
+            @Param("sapWoId") String sapWoId,
+            @Param("lotNumber") String lotNumber,
+            Pageable pageable
+    );
+
+
 
     public Page<PlanningWorkOrder> findAll(Specification<PlanningWorkOrder> spec, Pageable pageRequest);
 }
